dbdc935a3d54892c8077a4fb3acbe4e0614a6d27
fabianokafor369 <fabianokafor369@gmail.com>
2017-12-21 09:44:01 +0900

    Update bk.trees.py

--- bk.trees.py
+++ bk.trees.py
@@ -1,40 +1,40 @@
 def Levenshteind(a, b):
-  # Generate the length of both strings, which would be relevant in calculating the Levenshtein distance
-  lena, lenb = len(a), len(b)
+	# Generate the length of both strings, which would be relevant in calculating the Levenshtein distance
+	lena, lenb = len(a), len(b)
   
-  # Create a table for storing the solutions as done in dynamic programming
-  storage = [[0 for x in range(lenb+1)] for x in range(lena+1)]
+	# Create a table for storing the solutions as done in dynamic programming
+	storage = [[0 for x in range(lenb+1)] for x in range(lena+1)]
   
-  #Use dynamic programming to solve the problem in a bottom up fashion, by solving sub-problems
-  for i in range(lena+1):
-    for j in range(lenb+1):
+	#Use dynamic programming to solve the problem in a bottom up fashion, by solving sub-problems
+	for i in range(lena+1):
+    	for j in range(lenb+1):
 	
-      #fill up the base cases where either the first string or second string has length 0
-      #if the first string has a length of 0, then insertions equal to the length
-      #of the second string were done to get from the first string to the second.
-      if i == 0:
-        storage[i][j] = j
+		#fill up the base cases where either the first string or second string has length 0
+        #if the first string has a length of 0, then insertions equal to the length
+        #of the second string were done to get from the first string to the second.
+        	if i == 0:
+        		storage[i][j] = j
 		
-      #if the second string has a length of 0, then deletions equal in number to the
-      # length of the first string were done on the first string to get to the second string
-      elif j == 0:
-        storage[i][j] = i
+        #if the second string has a length of 0, then deletions equal in number to the
+        # length of the first string were done on the first string to get to the second string
+      		elif j == 0:
+        		storage[i][j] = i
 		
-      #if both strings have the same last characters, then no change was done on the last character
-      # of the first string to get to the last character of the second string. Therefore, the number
-      #of changes would be equal to the changes done on the remaining characters of the first string
-      #to get to the remaining characters of the second
-      elif a[i-1] == b[j-1]:
-        storage[i][j] = storage[i-1][j-1]
+        #if both strings have the same last characters, then no change was done on the last character
+        # of the first string to get to the last character of the second string. Therefore, the number
+        #of changes would be equal to the changes done on the remaining characters of the first string
+        #to get to the remaining characters of the second
+        	elif a[i-1] == b[j-1]:
+        		storage[i][j] = storage[i-1][j-1]
 		
-      #if both strings have different last characters, then a change was done to get from the first
-      # string to the second string. This change could either be an insertion, deletion or mutation,
-      # but never three of them at the same time. The change done will be 1 plus the previous record
-      # state of insertion, deletion or mutation
-      else:
-        storage[i][j] = 1 + min(storage[i][j - 1], storage[i - 1][j], storage[i - 1][j - 1])
+        #if both strings have different last characters, then a change was done to get from the first
+        # string to the second string. This change could either be an insertion, deletion or mutation,
+        # but never three of them at the same time. The change done will be 1 plus the previous record
+        # state of insertion, deletion or mutation
+      		else:
+        		storage[i][j] = 1 + min(storage[i][j - 1], storage[i - 1][j], storage[i - 1][j - 1])
 		
-  return storage[lena][lenb]
+    return storage[lena][lenb]
   
 Levenshteind(“aaaasss”, “as”)
 
